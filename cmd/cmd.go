988f816f562dc668cf0047c6f96b0598d8700c77
ss49919201 <sakashin10291029@gmail.com>
2023-04-01 00:01:14 +0900

    create cmd package

---
+++ cmd/cmd.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/cli/go-gh"
+)
+
+func Run() {
+	fmt.Println("hi world, this is the gh-unreqrev extension!")
+	client, err := gh.RESTClient(nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	response := struct{ Login string }{}
+	err = client.Get("user", &response)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("running as %s\n", response.Login)
+
+}
