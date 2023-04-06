29a954045479fea9bbb36f4ee2972b62f2853b98
ss49919201 <sakashin10291029@gmail.com>
2023-04-06 22:29:13 +0900

    remove reviewers from specific repository

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,13 +1,16 @@
 package cmd
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/cli/go-gh"
 )
 
 func Run() {
-	fmt.Println("hi world, this is the gh-unreqrev extension!")
 	client, err := gh.RESTClient(nil)
 	if err != nil {
 		fmt.Println(err)
@@ -15,9 +18,27 @@ func Run() {
 	}
 	response := struct{ Login string }{}
 	err = client.Get("user", &response)
+	ctx := context.Background()
+	b, err := json.Marshal(map[string]any{
+		"reviewers": []string{"ss49919201"},
+	})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("running as %s\n", response.Login)
+
+	resp := map[string]any{}
+	if err := client.DoWithContext(
+		ctx,
+		http.MethodDelete,
+		// TODO: カレントディレクトリのGitから取得
+		"repos/ss49919201/gh-unreqrev/pulls/1/requested_reviewers",
+		bytes.NewReader(b),
+		&resp,
+	); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%+v\n", resp["requested_reviewers"])
 }
